f18044b092823a5109ea75dc2d4af20b599e80ab
ducati-red916 <github.w@bermanhome.org>
2024-01-09 19:05:56 -0500

    tread

--- Centerstage_2023-24-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/android_studio_claws_simple.java
+++ Centerstage_2023-24-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/android_studio_claws_simple.java
@@ -7,16 +7,15 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-import java.util.concurrent.TimeUnit;
-
-@TeleOp(name = "android studio claws")
-public class android_studio_claws extends LinearOpMode {
+@TeleOp(name = "android studio claws simple")
+public class android_studio_claws_simple extends LinearOpMode {
 
     private DcMotor back_left_port_3;
     private DcMotor back_right_port_1;
     private DcMotor front_right_port_2;
     private DcMotor front_left_port_0;
     private DcMotor slide;
+    private DcMotor hanger;
     double drive_mode = 0.5;
     final double pickup1_close = 0.14;
     final double pickup1_open = 0;
@@ -50,6 +49,7 @@ public class android_studio_claws extends LinearOpMode {
         front_left_port_0 = hardwareMap.get(DcMotor.class, "front_left_port_0");
         limit_switch = hardwareMap.get(TouchSensor.class, "limit_switch");
         slide = hardwareMap.get(DcMotor.class, "slide");
+        hanger = hardwareMap.get(DcMotor.class, "hanger");
         plane = hardwareMap.get(Servo.class, "plane");
         pickup1 = hardwareMap.get(Servo.class, "pickup1");
         pickup2 = hardwareMap.get(Servo.class, "pickup2");
@@ -60,89 +60,53 @@ public class android_studio_claws extends LinearOpMode {
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                if (limit_switch.isPressed())
-                    pos0 = slide.getCurrentPosition();
                 drivemodes();
                 drive.run(gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.left_stick_x, drive_mode);
                 ((DcMotorEx)front_right_port_2).setVelocity(drive.FrontRight()*2700);
                 ((DcMotorEx) back_right_port_1).setVelocity(drive.BackRight()*2700);
                 ((DcMotorEx)front_left_port_0).setVelocity(drive.FrontLeft()*2700);
                 ((DcMotorEx) back_left_port_3).setVelocity(drive.BackLeft()*2700);
-                if (gamepad2.dpad_up && gamepad2.dpad_up != dpad_up_last){
-                    bottom_claw_toggle = !bottom_claw_toggle;
-                }
-                dpad_up_last = gamepad2.dpad_down;
-                if (gamepad2.dpad_down && gamepad2.dpad_down != dpad_down_last){
-                    top_claw_toggle = !top_claw_toggle;
-                }
-                dpad_down_last = gamepad2.dpad_down;
-
-               if (gamepad2.right_bumper)
+                if (gamepad1.right_bumper)
                     plane.setPosition(0);
-               else
+                else
                     plane.setPosition(0.65);
-               if (gamepad2.left_trigger != 0) {
-                   slide.setTargetPosition(slide.getTargetPosition() - Math.round(gamepad2.left_trigger * 50));
-                   slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                   ((DcMotorEx) slide).setVelocity(0.5);
-               } else {
-                   slide.setTargetPosition(slide.getTargetPosition() + Math.round(gamepad2.right_trigger * 50));
-                   slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                   ((DcMotorEx) slide).setVelocity(0.5);
-               }
-               if (gamepad2.a) {
-                   slide.setTargetPosition(pos0);
-                   slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                   ((DcMotorEx) slide).setVelocity(0.5);
-               }
-                if (gamepad2.y) {
-                    slide.setTargetPosition(pos0+500);
-                    slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    ((DcMotorEx) slide).setVelocity(0.5);
-                }
+                if (gamepad2.right_trigger != 0)
+                    slide.setPower(gamepad2.right_trigger);
+                else
+                    slide.setPower(-gamepad2.left_trigger);
+                if (gamepad1.right_trigger != 0)
+                    hanger.setPower(gamepad1.right_trigger);
+                else
+                    hanger.setPower(-gamepad1.left_trigger);
 
+                if (gamepad2.dpad_up){
+                    dropoff2.setPosition(dropoff2_drop);
+                } else if (gamepad2.dpad_right)
+                    dropoff2.setPosition(dropoff2_clear);
+                else
+                    dropoff2.setPosition(dropoff2_tranfer);
 
-                //1=claw, 2=rotater
-               if (gamepad2.left_bumper || !bottom_claw_toggle) {
-                   sleep(1000);
-                   pickup1.setPosition(pickup1_open);
-               } else {
-                   pickup1.setPosition(pickup1_close);
-               }
-                if (!(gamepad2.left_bumper || !bottom_claw_toggle))
-                    pickup1.setPosition(pickup1_close);
-               if (bottom_claw_toggle) {
-                   if (flag) {
-                       sleep(500);
-                       flag = false;
-                   }
-                   pickup2.setPosition(pickup2_up);
-               } else {
-                   pickup2.setPosition(pickup2_down);
-                   flag = true;
-               }
-                if (!(gamepad2.left_bumper || !bottom_claw_toggle))
-                    pickup1.setPosition(pickup1_close);
-               if (gamepad2.right_bumper)
+                if (gamepad2.dpad_down)
+                    pickup2.setPosition(pickup2_down);
+                else
+                    pickup2.setPosition(pickup2_up);
+
+                if (gamepad2.y)
                     dropoff1.setPosition(dropoff1_open);
-               else
+                else
                     dropoff1.setPosition(dropoff1_close);
 
-               if (!top_claw_toggle && !gamepad1.dpad_right)
-                    dropoff2.setPosition(dropoff2_tranfer);
-               else if (gamepad2.dpad_right)
-                   dropoff2.setPosition(dropoff2_clear);
-               else
-                    dropoff2.setPosition(dropoff2_drop);
+                if (gamepad2.a)
+                    pickup1.setPosition(pickup1_open);
+                else
+                    pickup1.setPosition(pickup1_close);
+
             }
         }
     }
 
     private void motormodes() {
-        slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // The next block resets the slide encoder to zero.  Make sure the slide is fully down before initializing the program.
-        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         back_left_port_3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         back_right_port_1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         front_left_port_0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
