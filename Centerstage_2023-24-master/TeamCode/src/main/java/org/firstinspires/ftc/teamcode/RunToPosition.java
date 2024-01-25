48eaa5c97ccd62c9e6b8f6dd9772af2c9cfccd17
ducati-red916 <github.w@bermanhome.org>
2024-01-24 19:59:07 -0500

    tread

--- Centerstage_2023-24-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToPosition.java
+++ Centerstage_2023-24-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToPosition.java
@@ -7,9 +7,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 public class RunToPosition {
 
     //constants
-    final private double WheelRadius = 1.89 / 2 * 2.54; //in Inches converted to CM
+    final private double WheelRadius = 1.88976/2; //diameter to radius
     final private double CMperTick = 2 * Math.PI * WheelRadius / 2000;
-    final private double NearRadius = 6;
+    final private double NearRadius = 24;
 
     //IMU stuffs
     //   BNO055IMU imu;
@@ -54,16 +54,16 @@ public class RunToPosition {
             //           atpo=false;
             if (Math.abs(DeltaY) < Math.abs(NearRadius)) {
 
-                Ypower = speed * Math.sin(DeltaY * Math.PI * 1/2 * 1/NearRadius); // start slowing when inside 24 inches
+                Ypower = -speed * Math.sin(DeltaY * Math.PI * 1/2 * 1/NearRadius); // start slowing when inside 24 inches
             } else {
-                Ypower = speed * (Math.abs(DeltaY) / DeltaY);
+                Ypower = -speed * (Math.abs(DeltaY) / DeltaY);
             }
         } else {
             if (Math.abs(DeltaX) > margin) {
                 if (DeltaX < Math.abs(NearRadius)) {
-                    Xpower = -speed * Math.sin(DeltaX * Math.PI * 1/2 * 1/NearRadius); // start slowing when inside 24 inches
+                    Xpower = speed * Math.sin(DeltaX * Math.PI * 1/2 * 1/NearRadius); // start slowing when inside 24 inches
                 } else {
-                    Xpower = -speed * (Math.abs(DeltaX) / DeltaX);
+                    Xpower = speed * (Math.abs(DeltaX) / DeltaX);
                 }
             } else {
                 Xpower = 0;
--- Centerstage_2023-24-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_auto.java
+++ Centerstage_2023-24-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_auto.java
@@ -37,6 +37,7 @@ public class test_auto extends LinearOpMode {
     RunToPosition gotopos;
     holonomic drive;
     int loopcounter = 0;
+    boolean flag = false;
 
     @Override
     public void runOpMode() {
@@ -73,7 +74,7 @@ public class test_auto extends LinearOpMode {
 
                 // Iterate through list and call a function to display info for each recognized object.
                 if (x==-1){
-                    while (loopcounter<250000) {
+                    while (loopcounter<150000) {
                         myTfodRecognitions = myTfodProcessor.getRecognitions();
                         for (Recognition myTfodRecognition_item2 : myTfodRecognitions) {
                             myTfodRecognition = myTfodRecognition_item2;
@@ -86,25 +87,57 @@ public class test_auto extends LinearOpMode {
                         telemetry.update();
                     }
 
-                    if (x <= 300 && !(x<1))
-                        position = 0;
-                    else if (x > 300)
-                        position = 1;
-                    else if (x == -1) {
-                        position = 2;
-                        x = 0;
+                    if (x==-1) {
+                        flag =true;
+                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), 0, -4, 1, 1);
+                        drive.run(gotopos.Xpower, gotopos.Ypower, 0, 0.2);
+                        ((DcMotorEx)front_right_port_2).setVelocity(drive.FrontRight()*2700);
+                        ((DcMotorEx)front_left_port_0).setVelocity(drive.FrontLeft()*2700);
+                        ((DcMotorEx) back_right_port_1).setVelocity(drive.BackRight()*2700);
+                        ((DcMotorEx)back_left_port_3).setVelocity(drive.BackLeft()*2700);
+                        loopcounter = 0;
+                        while (loopcounter < 150000) {
+                            myTfodRecognitions = myTfodProcessor.getRecognitions();
+                            for (Recognition myTfodRecognition_item2 : myTfodRecognitions) {
+                                myTfodRecognition = myTfodRecognition_item2;
+                                x = (myTfodRecognition.getLeft() + myTfodRecognition.getRight()) / 2;
+                                y = (myTfodRecognition.getTop() + myTfodRecognition.getBottom()) / 2;
+                            }
+                            telemetry.addData("X", x);
+                            telemetry.addData("Y", y);
+                            telemetry.addData("loopcounter", loopcounter++);
+                            telemetry.update();
+                        }
                     }
+                        if (x <= 300 && !(x < 1))
+                            position = 0;
+                        else if (x > 300)
+                            position = 1;
+                        else if (x == -1) {
+                            position = 2;
+                            x = 0;
+                        }
+
                 }
 
-
+                if (flag=true) {
                     if (position == 0)
-                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), 12, -12, 1, 1);
+                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), 6, -20, 1, 1);
                     else if (position == 1)
-                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), 0, -12, 1, 1);
+                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), 0, -24, 1, 2);
                     else if (position == 2)
-                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), -12, -12, 1, 1);
+                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), -8, -23, 1, 2);
                     //else if (position == 2)
-                        //gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), -12, -12, 1, 1);
+                    //gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), -12, -12, 1, 1);
+                }else {
+                    if (position == 0)
+                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), 6, -24, 1, 1);
+                    else if (position == 1)
+                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), 0, -28, 1, 2);
+                    else if (position == 2)
+                        gotopos.Run(dead_right_port_1.getCurrentPosition(), dead_back_port_2.getCurrentPosition(), -8, -27, 1, 2);
+                }
+
 
                 drive.run(gotopos.Xpower, gotopos.Ypower, 0, 0.2);
                 ((DcMotorEx)front_right_port_2).setVelocity(drive.FrontRight()*2700);
@@ -146,7 +179,7 @@ public class test_auto extends LinearOpMode {
         // First, create a TfodProcessor.Builder.
         myTfodProcessorBuilder = new TfodProcessor.Builder();
         // Set the name of the file where the model can be found.
-        myTfodProcessorBuilder.setModelFileName("model_20231226_191211.tflite");
+        myTfodProcessorBuilder.setModelFileName("model_cameravisionV2.tflite");
         // Set the full ordered list of labels the model is trained to recognize.
         myTfodProcessorBuilder.setModelLabels(JavaUtil.createListWith("BLUE Elm", "RED Elm"));
         // Set the aspect ratio for the images used when the model was created.
